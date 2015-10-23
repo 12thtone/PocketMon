cc4c119a0ec44231aba9b2c74694feae57056912
Matt Maher <maher@12thtone.com>
2015-10-23 10:25:46 -0400

    Initial Commit

---
+++ pocketmon/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pocketmon
+//
+//  Created by Matthew Maher on 10/23/15.
+//  Copyright © 2015 Matthew Maher. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
